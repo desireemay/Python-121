fae51c61e59cc3486e59597dd45a6d3d372032c6
desireemay <desireeicaro392@gmail.com>
2025-11-20 08:38:09 +0800

    Create Polymorphism.py

---
+++ Concepts of OOP/Polymorphism.py
@@ -0,0 +1,28 @@
+Printers
+class InkPrinter:
+    def print_document(self):
+        return "Printing using ink..."
+
+class LaserPrinter:
+    def print_document(self):
+        return "Printing using laser..."
+
+printers = [InkPrinter(), LaserPrinter()]
+for p in printers:
+    print(p.print_document())
+
+
+Duck Typing
+def make_it_speak(obj):
+    obj.speak()
+
+class Bird:
+    def speak(self):
+        print("Tweettt tweet!")
+
+class Robot:
+    def speak(self):
+        print("Beepppp!")
+
+make_it_speak(Bird())
+make_it_speak(Robot())
