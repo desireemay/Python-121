b75aaf1d88c4c259aeb47e384e16c9e89d1768d0
desireemay <desireeicaro392@gmail.com>
2025-11-20 08:12:22 +0800

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,39 @@
+class Animal:
+    def speak(self, speak):
+        print("their sounds")
+
+class Dog(Animal):
+    def speak(self):
+        print("Wooooof wooff")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow Meow!")
+
+Dog().speak()
+Cat().speak()
+
+
+
+
+class Vehicle:
+    def __init__(self, brand, fuel):
+        self.brand = brand
+        self.fuel = fuel
+
+class Car(Vehicle):
+    def __init__(self, brand, fuel, doors):
+        Vehicle.__init__(self, brand, fuel)
+        self.doors = doors
+
+    def drive(self):
+        if self.fuel > 0:
+            self.fuel -= 1
+            print(f"The {self.brand} car is driving... Fuel left: {self.fuel}")
+        else:
+            print(f"The {self.brand} car has no fuel left!")
+            
+my_car = Car("BMW", 5, 4)
+my_car.drive()
+my_car.drive()
+my_car.drive()
