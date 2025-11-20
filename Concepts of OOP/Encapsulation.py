a3f95892b3939af4cf44fb5f37c2b4da12d2e259
desireemay <desireeicaro392@gmail.com>
2025-11-20 08:33:11 +0800

    Create Encapsulation.py

---
+++ Concepts of OOP/Encapsulation.py
@@ -0,0 +1,42 @@
+Bank Account
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner=owner
+        self.balance= balance
+        
+    def deposit(self, amount):
+        self.balance += amount
+        print (f"{self.owner} deposited Php {amount}. New balance: Php{self.balance}")
+        
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"{self.owner} withdrew Php{amount}. Remaining balance: Php {self.balance}")
+        else:
+            print("Insuficient funds!")
+
+amount1= BankAccount("Des", 5000)
+amount1.deposit(1500)
+amount1.withdraw(2000)
+amount1.withdraw(100001)
+
+
+Age Validation
+class Person:
+    def __init__(self):
+        self.__age = 0
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value > 0:
+            self.__age = value
+        else:
+            raise ValueError("Age must be a positive integer")
+
+p = Person()
+p.age = 19
+print(p.age)
